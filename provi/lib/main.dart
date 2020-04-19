f5525bd1c2b394bda71688c232c9931046e57f71
AdamMusa <adammusa2222@gmail.com>
2020-04-19 13:57:06 +0100

    added provider model and two counters

--- provi/lib/main.dart
+++ provi/lib/main.dart
@@ -2,33 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:provi/provider_model.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-  MaterialApp(home:ChangeNotifierProvider(
-    create: (context)=>Counter(),
-    child: MyApp(),
-    ))
-);
+void main() => runApp(MaterialApp(
+        home: ChangeNotifierProvider(
+      create: (context) => Counter(),
+      child: MyApp(),
+    )));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final counter = Provider.of<Counter>(context);
     return Scaffold(
-      appBar: AppBar(
-        title:Text('Provier count')
+      appBar: AppBar(title: Text('Provier count')),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                      width: 120,
+                      height: 100,
+                      color: Colors.grey,
+                      child: Center(
+                          child: Text(
+                        '${counter.count_a}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        textScaleFactor: 1.5,
+                      ))),
+                  Container(
+                      width: 120,
+                      height: 100,
+                      color: Colors.grey,
+                      child: Center(
+                          child: Text(
+                        '${counter.count_b}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        textScaleFactor: 1.5,
+                      ))),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                  width: 120,
+                  height: 100,
+                  color: Colors.grey,
+                  child: Center(
+                      child: Text(
+                    '${counter.somme()}',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    textScaleFactor: 1.5,
+                  ))),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.add), 
+                    onPressed: counter.increment_a,
+                    tooltip: 'Counter for a',
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add), 
+                    onPressed: counter.increment_b,
+                    tooltip: 'Counter for b',
+                  )
+                ],
+              )
+            ]),
       ),
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:<Widget>[
-          Text('Vous avez appuyé : '),
-          Text('${counter.count}')
-        ]
-      ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counter.increment,
-        child: Icon(Icons.add),
-      )
     );
   }
 }
--- provi/lib/provider_model.dart
+++ provi/lib/provider_model.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
 class Counter with ChangeNotifier{
-  int count = 0;
+  int count_a = 0;
+  int count_b = 0;
 
-  void increment(){
-    count++;
+  void increment_a(){
+    count_a++;
     notifyListeners();
+  }
+  void increment_b(){
+    count_b++;
+    notifyListeners();
+  }
+  int somme(){
+    return count_a + count_b;
   }
 }
